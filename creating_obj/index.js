ba08dfd72a17b92851eef4547aca655110d3636b
prabhu123mishra <iamprabhakarmishra1@gmail.com>
2024-05-19 21:33:03 +0530

    first commit

---
+++ creating_obj/index.js
@@ -0,0 +1,85 @@
+// console.log("prbhkr");
+
+// function Rectangle(len,bre) {
+
+//      this.length=len;
+//      this.breadth=bre;
+
+//       this.draw=function(){
+//         console.log('drawing');
+//       }
+
+// }
+// let createobject1=new Rectangle(4,5);
+// let createobject3=new Rectangle(5,9);
+
+//  function createRectangle(len,bre){
+
+//     return Rectangle ={
+//         len,
+//         bre,
+
+//          draw() {
+//             console.log('drawpen');
+//          }
+//     };
+
+//  }
+//  let rectangleobj1=createRectangle(5,3);
+
+// let numbers = [1,2,5,8,9,3];
+
+//  console.log(numbers);
+
+//  console.log(numbers.indexOf(2));
+
+//  console.log(numbers.includes(6));
+//  console.log(numbers);
+
+//  console.log(numbers.indexOf(8,2));
+
+// let courses = [
+//    {no:1,naam:'prbhkr'},
+//    {no:2,naam:'mishra'}
+
+// ];
+// console.log(courses);
+// let course = courses.find(function(course){
+//    return course.naam=='prbhkr';
+// })
+// console.log(course);
+
+// let course = courses.find(function(course){
+//    return course.naam==='prbhkr';
+// })
+// console.log(course);
+
+// let course=courses.find(course =>course.naam==='mishra');
+// console.log(course);
+
+// let numbers=[1,3,-5,-6];
+
+// let filtered = numbers.filter(value => value>=0);
+// console.log(filtered);
+
+// let numbers=[7,8,9,10];
+// console.log(numbers);
+
+// let items =numbers.map(value =>'student_no'+value);
+// console.log(items);
+
+// let numbers =[7,8,-5,-2];
+
+// let filtered = numbers.filter(val => val>=0);
+
+// let items =filtered.map(function(num){
+//    return {value:num};
+// })
+// console.log(items);
+
+// let numbers =[7,8,-5,-2];
+
+// let items =numbers
+//               .filter(val => val>=0)
+//               .map(num =>{value:num});
+// console.log(items);
---
+++ promise/promise.js
@@ -0,0 +1,17 @@
+// setTimeout (function(){
+//     console.log('i am bhargavi kumari');
+// },5000);
+
+// function sync (){
+//     console.log('i am prbhkr mishra');
+// }
+
+//  sync();
+//  console.log('i am bhaskar mishra');
+let myPrimses = new Promise (function(resolve,reject) {
+setTimeout(function(){
+    console.log('i m shankar mishra');
+},5000);
+resolve(2000);
+});
+console.log('MAA');
